aa87b46a651cd49c9e097bd0240d788fb462cba7
Dante Calderón <mail@dantecalderon.dev>
2020-09-05 20:19:03 -0500

    feat: Add enums and constants (#2)
    * Add http status codes and methods supported
    
    * Export all from use a simple import
    
    * Add constants for reflect metadata keys

---
+++ src/common/enums/http-methods.ts
@@ -0,0 +1,13 @@
+/**
+ * List of supported Http Methods
+ */
+export enum HttpMethod {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
+  ALL = 'all',
+  PATCH = 'patch',
+  OPTIONS = 'options',
+  HEAD = 'head'
+}
