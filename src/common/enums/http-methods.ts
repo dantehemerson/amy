c1122fc82ae90c3a2409252e15db16cf31058005
Dante Calderon <mail@dantecalderon.dev>
2020-09-05 20:11:28 -0500

    Add http status codes and methods supported

---
+++ src/common/enums/http-methods.ts
@@ -0,0 +1,14 @@
+/**
+ * List of supported Http Methods
+ * @package Neo
+ */
+export enum HttpMethod {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
+  ALL = 'all',
+  PATCH = 'patch',
+  OPTIONS = 'options',
+  HEAD = 'head'
+}
